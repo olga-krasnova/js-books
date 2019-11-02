486355064cba8f85f7f3e87da77152e0bdb8f136
olga-krasnova <olga.krasnova@flatstack.com>
2019-11-02 09:52:48 +0300

    fix

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -4,6 +4,7 @@ export default class ApiService {
   constructor() {
     this.instance = axios.create({
       baseURL: process.env.REACT_APP_API_URL,
+
     });
 
     Object.assign(this, this.instance);
